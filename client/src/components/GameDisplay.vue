84f5fc7f73848f88ca8332be17655d14cb35d97c
Aaron Wilson <aaronnw11@gmail.com>
2019-11-03 00:40:28 -0500

    created initial project with vue and flask api

---
+++ client/src/components/GameDisplay.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <h1>{{ home_team.name }} vs. {{away_team.name}}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'GameDisplay',
+  props: {
+    game_id: String
+  },
+  data() {
+    return {
+      time: '',
+      location: '',
+      tv: '', 
+      home_team: {
+        name: '',
+        record: '',
+        primary_color: '',
+        secondary_color: '',
+        logo: '',
+
+      },
+      away_team: {
+        name: '',
+        record: '',
+        primary_color: '',
+        secondary_color: '',
+        logo: '',
+      },
+    };
+  },
+  created() {
+    console.log('todo: query team info')
+  }
+}
+</script>
